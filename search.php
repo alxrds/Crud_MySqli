bf9ac9a0830feee18349a2ebd3736d04ecd8caaa
Alexandre Rodrigues dos Santos <54284137+alxrds@users.noreply.github.com>
2020-12-16 16:43:24 -0300

    Update search.php
    correção

--- search.php
+++ search.php
@@ -4,7 +4,7 @@ include 'connection.php';
 
 if($_GET){
 
-    $search = mysqli_real_escape_string($conn, $_GET['q']);
+    $search = $conn->real_escape_string($_GET['q']);
 
     $sql = "SELECT * FROM tbl_usuario WHERE nome LIKE '%$search%' OR sobrenome LIKE '%$search%'";
     $stmt = $conn->query($sql);
@@ -19,5 +19,3 @@ if($_GET){
 }else{
     echo 'Campo de busca vazio';
 }
-
-?>
